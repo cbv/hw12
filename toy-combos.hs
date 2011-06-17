da96af2d24a2f1847fac9aaebaac90b1928576ed
bblum <bblum@b3d0796d-73f4-44f3-a614-c7306c265f62>
2011-06-17 22:45:13 +0000

    enhance toy-combos.hs
    git-svn-id: svn://R_E_D_A_C_T_E_D/icfp/hw12@116 b3d0796d-73f4-44f3-a614-c7306c265f62

--- toy-combos.hs
+++ toy-combos.hs
@@ -1,4 +1,6 @@
 import Prelude hiding (succ)
+import Control.Applicative
+import Control.Monad.State
 
 data Exp = Zero
          | Succ Exp
@@ -9,8 +11,8 @@ data Exp = Zero
 
 shownum :: Exp -> Maybe Int
 shownum Zero = Just 0
-shownum (Succ e) = fmap (+1) $ shownum e
-shownum (Dbl e) = fmap (*2) $ shownum e
+shownum (Succ e) = (+1) <$> shownum e
+shownum (Dbl e) = (*2) <$> shownum e
 shownum _ = Nothing
 
 instance Show Exp where
@@ -49,15 +51,21 @@ subst e1 x e2@(Var y) = if x == y then e1 else e2
 subst e1 x e2@(Lam y e3) = if x == y then e2 else Lam y $ subst e1 x e3
 subst e1 x e2@(App e3 e4) = App (subst e1 x e3) (subst e1 x e4)
 
+type Counter a = State Int a
+
 -- eval
-eval :: Exp -> Exp
-eval (Succ e) = Succ $ eval e
-eval (Dbl e) = Dbl $ eval e
+eval :: Exp -> Counter Exp
+eval (Succ e) = Succ <$> eval e
+eval (Dbl e) = Dbl <$> eval e
 eval (App e1 e2) =
-    case eval e1 of
-        Lam x e1' -> eval $ subst (eval e2) x e1'
-        e1' -> error $ "tried to eval " ++ show e1
-eval e = e
+    do e1' <- eval e1
+       case e1' of
+           Lam x e1'' ->
+               do modify (+1)
+                  e2' <- eval e2
+                  eval $ subst e2' x e1''
+           _ -> error $ "tried to eval " ++ show e1
+eval e = return e
 
 -- basic combinators/etc
 
@@ -80,7 +88,7 @@ basefour =
     in Lam "x" $ Var "x" $$ f $$ i $$ (Var "x" $$ (t $$ a) $$ b $$ i)
 
 -- \x y z w -> get 0 w $ quad $ get 0 z $ quad $ get 0 y $ quad $ get 0 x
-gen256 =
+timecube =
     let quad = Dbl . Dbl
     in Lam "x" $ Lam "y" $ Lam "z" $ Lam "w" $
         App (basefour $$ Var "x") $ quad $
@@ -127,7 +135,47 @@ nums = [
         [t, t, s, i], [i, t, s, i], [f, t, s, i], [s, t, s, i],
         [t, i, s, i], [i, i, s, i], [f, i, s, i], [s, i, s, i],
         [t, f, s, i], [i, f, s, i], [f, f, s, i], [s, f, s, i],
-        [t, s, s, i], [i, s, s, i], [f, s, s, i], [s, s, s, i]
-       ]
+        [t, s, s, i], [i, s, s, i], [f, s, s, i], [s, s, s, i],
 
-nums' = map (\[x,y,z,w] -> eval $ gen256 $$ x $$ y $$ z $$ w) nums
+        [t, t, t, f], [i, t, t, f], [f, t, t, f], [s, t, t, f], -- 128-255
+        [t, i, t, f], [i, i, t, f], [f, i, t, f], [s, i, t, f],
+        [t, f, t, f], [i, f, t, f], [f, f, t, f], [s, f, t, f],
+        [t, s, t, f], [i, s, t, f], [f, s, t, f], [s, s, t, f],
+
+        [t, t, i, f], [i, t, i, f], [f, t, i, f], [s, t, i, f],
+        [t, i, i, f], [i, i, i, f], [f, i, i, f], [s, i, i, f],
+        [t, f, i, f], [i, f, i, f], [f, f, i, f], [s, f, i, f],
+        [t, s, i, f], [i, s, i, f], [f, s, i, f], [s, s, i, f],
+
+        [t, t, f, f], [i, t, f, f], [f, t, f, f], [s, t, f, f],
+        [t, i, f, f], [i, i, f, f], [f, i, f, f], [s, i, f, f],
+        [t, f, f, f], [i, f, f, f], [f, f, f, f], [s, f, f, f],
+        [t, s, f, f], [i, s, f, f], [f, s, f, f], [s, s, f, f],
+
+        [t, t, s, f], [i, t, s, f], [f, t, s, f], [s, t, s, f],
+        [t, i, s, f], [i, i, s, f], [f, i, s, f], [s, i, s, f],
+        [t, f, s, f], [i, f, s, f], [f, f, s, f], [s, f, s, f],
+        [t, s, s, f], [i, s, s, f], [f, s, s, f], [s, s, s, f],
+
+        [t, t, t, s], [i, t, t, s], [f, t, t, s], [s, t, t, s],
+        [t, i, t, s], [i, i, t, s], [f, i, t, s], [s, i, t, s],
+        [t, f, t, s], [i, f, t, s], [f, f, t, s], [s, f, t, s],
+        [t, s, t, s], [i, s, t, s], [f, s, t, s], [s, s, t, s],
+
+        [t, t, i, s], [i, t, i, s], [f, t, i, s], [s, t, i, s],
+        [t, i, i, s], [i, i, i, s], [f, i, i, s], [s, i, i, s],
+        [t, f, i, s], [i, f, i, s], [f, f, i, s], [s, f, i, s],
+        [t, s, i, s], [i, s, i, s], [f, s, i, s], [s, s, i, s],
+
+        [t, t, f, s], [i, t, f, s], [f, t, f, s], [s, t, f, s],
+        [t, i, f, s], [i, i, f, s], [f, i, f, s], [s, i, f, s],
+        [t, f, f, s], [i, f, f, s], [f, f, f, s], [s, f, f, s],
+        [t, s, f, s], [i, s, f, s], [f, s, f, s], [s, s, f, s],
+
+        [t, t, s, s], [i, t, s, s], [f, t, s, s], [s, t, s, s],
+        [t, i, s, s], [i, i, s, s], [f, i, s, s], [s, i, s, s],
+        [t, f, s, s], [i, f, s, s], [f, f, s, s], [s, f, s, s],
+        [t, s, s, s], [i, s, s, s], [f, s, s, s], [s, s, s, s]
+        ]
+
+nums' = map (\[x,y,z,w] -> runState (eval $ timecube $$ x $$ y $$ z $$ w) 0) nums
