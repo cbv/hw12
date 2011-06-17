b2e811601b79ba98c567ab92877fe771d5486632
bblum <bblum@b3d0796d-73f4-44f3-a614-c7306c265f62>
2011-06-17 18:34:39 +0000

    update toy-combos
    git-svn-id: svn://R_E_D_A_C_T_E_D/icfp/hw12@64 b3d0796d-73f4-44f3-a614-c7306c265f62

--- toy-combos.hs
+++ toy-combos.hs
@@ -1,14 +1,14 @@
 import Prelude hiding (succ)
 
-data Exp = Val Int
-         | Func (Exp -> Exp)
+data Exp = Zero
+         | Succ Exp
          | Var String
          | Lam String Exp
          | App Exp Exp
 
 instance Show Exp where
-    show (Val i) = show i
-    show (Func f) = "<fn>"
+    show (Zero) = "0"
+    show (Succ e) = "1+" ++ show e
     show (Var s) = s
     show (Lam s e) = "(\\" ++ s ++ "." ++ show e ++ ")"
     show (App e1 (App e2 e3)) = show e1 ++ " (" ++ show e2 ++ " " ++ show e3 ++ ")"
@@ -19,8 +19,8 @@ a $$ b = App a b
 
 -- [e1/x]e2
 subst :: Exp -> String -> Exp -> Exp
-subst e1 x e2@(Val _) = e2
-subst e1 x e2@(Func _) = e2
+subst e1 x Zero = Zero
+subst e1 x (Succ e) = Succ $ subst e1 x e
 subst e1 x e2@(Var y) = if x == y then e1 else e2
 subst e1 x e2@(Lam y e3) = if x == y then e2 else Lam y $ subst e1 x e3
 subst e1 x e2@(App e3 e4) = App (subst e1 x e3) (subst e1 x e4)
@@ -29,9 +29,9 @@ subst e1 x e2@(App e3 e4) = App (subst e1 x e3) (subst e1 x e4)
 eval :: Exp -> Exp
 eval (App e1 e2) =
     case eval e1 of
-        Func e1' -> e1' $ eval e2
         Lam x e1' -> subst (eval e2) x e1'
-        e1' -> error $ "tried to eval " ++ show e1'
+        e1' -> error $ "tried to eval " ++ show e1
+eval (Succ e) = Succ $ eval e
 eval e = e
 
 -- basic combinators/etc
@@ -45,13 +45,9 @@ t = Lam "x" $ Lam "y" $ Var "x"
 
 f = Lam "x" $ Lam "y" $ Var "y"
 
-succ (Val x) = Val $ x + 1
-succ e = error $ "tried to succ " ++ show e
-
-dbl (Val x) = Val $ x * 2
-dbl e = error $ "tried to dbl " ++ show e
-
 -- Interesting combinators below
 
 basefour x =
-    x $$ f $$ i $$ (x $$ (t $$ Func succ) $$ (Func $ succ . succ) $$ i)
+    let a = Lam "f" $ Lam "x" $ Succ $ Var "f" $$ Var "x"
+        b = Lam "f" $ Lam "x" $ Succ $ Succ $ Var "f" $$ Var "x"
+    in x $$ f $$ i $$ (x $$ (t $$ a) $$ b $$ i)
